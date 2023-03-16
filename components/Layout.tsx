3e83f2ab5893560099a233e70873c28cdcd630af
myot <myo.t@rznet.com>
2023-03-16 17:57:53 +0700

    update ui

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,8 +4,8 @@ export default function Layout({ children }) {
   return (
     <div className='flex flex-col h-screen'>
       <header className="flex shadow-lg p-4 bg-gray-900 text-white">
-        <Link href="/graphql-request">
-           GraphQL Request
+        <Link href="/">
+           Home
         </Link>
       </header>
       <div className='flex-1 p-16 flex justify-center items-center'> 
